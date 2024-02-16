33863264a761178293a46954bf690306d7041e12
jigsaw <jigsaw11211@gmail.com>
2024-02-17 01:15:45 +0200

    update requirements.txt

--- BrainBites/__init__.py
+++ BrainBites/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, unicode_literals
-# Это будет гарантировать, что приложение всегда импортируется при запуске Django
 from BrainBites.celery_config import app as celery_app
 
 __all__ = ('celery_app',)
--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn BrainBites.wsgi --log-file - --timeout 190
+worker: celery -A BrainBites worker --loglevel=info
+beat: celery -A BrainBites beat --loglevel=info
