1f1dd7380791145b2ebe10b65c91a642aef30665
jigsaw <jigsaw11211@gmail.com>
2024-04-08 00:08:52 +0300

    fix

--- BrainBites/celery_config.py
+++ BrainBites/celery_config.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import, unicode_literals
 import os
+import ssl
+
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BrainBites.settings')
@@ -9,5 +11,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BrainBites.settings')
 app = Celery('BrainBites')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.broker_use_ssl = {'ssl_cert_reqs': ssl.CERT_NONE}
 
 app.autodiscover_tasks()
